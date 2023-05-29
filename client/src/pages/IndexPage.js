95ad385e7e8e36446737d75b501254de3ae5f237
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-29 18:38:31 +0300

    added pages

---
+++ client/src/pages/IndexPage.js
@@ -0,0 +1,11 @@
+import Post from "../post";
+
+export default function IndexPage() {
+    return (
+        <>
+            <Post />
+            <Post />
+            <Post />
+        </>
+    )
+}
---
+++ client/src/pages/LoginPage.js
@@ -0,0 +1,5 @@
+export default function LoginPage() {
+    return (
+        <div>You are Online...</div>
+    )
+}
