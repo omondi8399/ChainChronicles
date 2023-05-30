b36cceb2ee5a4db017280bbb035e8e0f95b0cabc
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-30 11:24:51 +0300

    Updated App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,15 @@ import './App.css';
 import Layout from './Layout';
 import IndexPage from './pages/IndexPage';
 import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Layout />}>
-      <Route index element={
-        <IndexPage />
-      } />
-      <Route path={'/login'} element={
-        <LoginPage />
-      } />
+      <Route index element={<IndexPage />} />
+      <Route path={'/login'} element={<LoginPage />} />
+      <Route path={'/register'} element={<RegisterPage/>} />
       </Route>
     </Routes>
     );
