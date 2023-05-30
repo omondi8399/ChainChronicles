f4551f77d82928a4044a427dddc1a453e35581e2
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-30 12:26:24 +0300

    Updated login page

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,9 +1,21 @@
+import { useState } from "react"
+
 export default function LoginPage() {
+    const [username,setUsername] = useState('')
+    const [password,setPassword] = useState('')
+    async function login(e) {
+        e.preventDefault()
+        await fetch('http://localhost:4000/login', {
+            method: 'POST',
+            body: JSON.stringify({username,password}),
+            headers: {'content-Type': 'application/json'},
+        })
+    }
     return (
-        <form className="login">
+        <form className="login" onSubmit={login}>
             <h1>Login</h1>
-            <input type="text" placeholder="username" />
-            <input type="password" placeholder="password" />
+            <input type="text" placeholder="username" value={username} onChange={e => setUsername(e.target.value)}/>
+            <input type="password" placeholder="password" value={password} onChange={e => setPassword(e.target.value)} />
             <button>Login</button>
         </form>
     )
