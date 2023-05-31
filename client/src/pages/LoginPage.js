108485421e6d469bb01c6e04bbc29b1eec91dcfc
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-31 11:21:40 +0300

    Updated Login Page

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,16 +1,34 @@
-import { useState } from "react"
+import { useContext, useState } from "react"
+import { Navigate } from "react-router-dom"
+import { UserContext } from "../UserContext"
 
 export default function LoginPage() {
     const [username,setUsername] = useState('')
     const [password,setPassword] = useState('')
+    const [redirect,setRedirect] = useState(false)
+    const {setUserInfo} = useContext(UserContext)
     async function login(e) {
         e.preventDefault()
-        await fetch('http://localhost:4000/login', {
+        const response = await fetch('http://localhost:4000/login', {
             method: 'POST',
             body: JSON.stringify({username,password}),
             headers: {'content-Type': 'application/json'},
+            credentials: 'include',
         })
+        if (response.ok) {
+            response.json().then(userInfo => {
+                setUserInfo(userInfo)
+                setRedirect(true)
+            })
+            setRedirect(true)
+        } else {
+            alert('wrong credentials')
+        }
     }
+
+        if (redirect) {
+            return <Navigate to={'/'} />
+        }
     return (
         <form className="login" onSubmit={login}>
             <h1>Login</h1>
