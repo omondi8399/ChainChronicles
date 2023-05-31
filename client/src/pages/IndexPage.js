8b93aaa1fce6fccd336bd31fa92baf6278f51d91
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-31 17:19:40 +0300

    updated Index Page

--- client/src/pages/IndexPage.js
+++ client/src/pages/IndexPage.js
@@ -1,11 +1,20 @@
-import Post from "../post";
+import Post from "../Post";
+import {useEffect, useState} from "react";
 
 export default function IndexPage() {
-    return (
-        <>
-            <Post />
-            <Post />
-            <Post />
-        </>
-    )
+const [posts,setPosts] = useState([]);
+useEffect(() => {
+    fetch('http://localhost:4000/post').then(response => {
+    response.json().then(posts => {
+        setPosts(posts);
+    });
+    });
+}, []);
+return (
+    <>
+    {posts.length > 0 && posts.map(post => (
+        <Post {...post} />
+    ))}
+    </>
+);
 }
