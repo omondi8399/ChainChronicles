af1ac5ded10337d8333057b00f11f49306640776
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-31 17:21:37 +0300

    updated Header.js

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,46 +1,46 @@
-import { useContext, useEffect } from "react";
-import { Link } from "react-router-dom";
-import { UserContext } from "./UserContext";
+import {Link} from "react-router-dom";
+import {useContext, useEffect, useState} from "react";
+import {UserContext} from "./UserContext";
 
 export default function Header() {
-    const {setUserInfo, userInfo} = useContext(UserContext)
-    useEffect(() => {
-        fetch('http://localhost:4000/profile', {
-            credentials: 'include',
-        }).then(response => {
-            response.json().then(userInfo => {
-                setUserInfo(userInfo)
-            })
-        })
-    }, [])
+const {setUserInfo,userInfo} = useContext(UserContext);
+useEffect(() => {
+    fetch('http://localhost:4000/profile', {
+    credentials: 'include',
+    }).then(response => {
+    response.json().then(userInfo => {
+        setUserInfo(userInfo);
+    });
+    });
+}, []);
 
-    function logout() {
-        fetch('http://localhost:4000/logout', {
-            credentials: 'include',
-            method: 'POST',
-        })
-            setUserInfo(null)
-    }
+function logout() {
+    fetch('http://localhost:4000/logout', {
+    credentials: 'include',
+    method: 'POST',
+    });
+    setUserInfo(null);
+}
 
-    const username = userInfo?.username
+const username = userInfo?.username;
 
-    return(
-        <header>
-        <Link to="/" className="logo">ChainChronicles</Link>
-        <nav>
-            {username && (
-                <>
-                    <Link to="/Create">Create new post</Link>
-                    <a onClick={logout}>Logout</a>
-                </>
-            )}
-            {!username && (
-                <>
-                    <Link to="/login">Login</Link>
-                    <Link to="/register">Register</Link>
-                </>
-            )}
-        </nav>
-        </header>
-    )
+return (
+    <header>
+    <Link to="/" className="logo">ChainChronicles</Link>
+    <nav>
+        {username && (
+        <>
+            <Link to="/create">Create new post</Link>
+            <a onClick={logout}>Logout ({username})</a>
+        </>
+        )}
+        {!username && (
+        <>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+        </>
+        )}
+    </nav>
+    </header>
+);
 }
