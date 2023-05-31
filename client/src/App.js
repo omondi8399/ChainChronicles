f825593654bfae8f35e3b370354a7f782b4af34b
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-31 17:22:08 +0300

    updated App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,31 @@
-import {Route, Routes} from 'react-router-dom';
 import './App.css';
-import Layout from './Layout';
-import IndexPage from './pages/IndexPage';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
+import Post from "./Post";
+import Header from "./Header";
+import {Route, Routes} from "react-router-dom";
+import Layout from "./Layout";
+import IndexPage from "./pages/IndexPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import {UserContextProvider} from "./UserContext";
+import CreatePost from "./pages/CreatePost";
+import PostPage from "./pages/PostPage";
+import EditPost from "./pages/EditPost";
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Layout />}>
-      <Route index element={<IndexPage />} />
-      <Route path={'/login'} element={<LoginPage />} />
-      <Route path={'/register'} element={<RegisterPage/>} />
-      </Route>
-    </Routes>
-    );
+    <UserContextProvider>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<IndexPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/create" element={<CreatePost />} />
+          <Route path="/post/:id" element={<PostPage />} />
+          <Route path="/edit/:id" element={<EditPost />} />
+        </Route>
+      </Routes>
+    </UserContextProvider>
+  );
 }
 
 export default App;
