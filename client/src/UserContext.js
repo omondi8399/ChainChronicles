2be450fad0408de74fadd6cad9623a5f89250134
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-31 17:20:36 +0300

    updated UserContext.js

--- client/src/UserContext.js
+++ client/src/UserContext.js
@@ -1,12 +1,12 @@
-import { createContext, useState } from "react";
+import {createContext, useState} from "react";
 
-export const UserContext = createContext({})
+export const UserContext = createContext({});
 
 export function UserContextProvider({children}) {
-    const [userInfo,setUserInfo] = useState({})
-    return (
-        <UserContext.Provider value={{userInfo,setUserInfo}}>
-            {children}
-        </UserContext.Provider>
-    )
+const [userInfo,setUserInfo] = useState({});
+return (
+    <UserContext.Provider value={{userInfo,setUserInfo}}>
+    {children}
+    </UserContext.Provider>
+);
 }
