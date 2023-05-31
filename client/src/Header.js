eeadfb2792dec7dfa9bab2d90d64b45c2488cb60
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-31 11:22:13 +0300

    Updated Header

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,12 +1,45 @@
+import { useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { UserContext } from "./UserContext";
 
 export default function Header() {
+    const {setUserInfo, userInfo} = useContext(UserContext)
+    useEffect(() => {
+        fetch('http://localhost:4000/profile', {
+            credentials: 'include',
+        }).then(response => {
+            response.json().then(userInfo => {
+                setUserInfo(userInfo)
+            })
+        })
+    }, [])
+
+    function logout() {
+        fetch('http://localhost:4000/logout', {
+            credentials: 'include',
+            method: 'POST',
+        })
+            setUserInfo(null)
+    }
+
+    const username = userInfo?.username
+
     return(
         <header>
         <Link to="/" className="logo">ChainChronicles</Link>
         <nav>
-            <Link to="/login">Login</Link>
-            <Link to="/register">Register</Link>
+            {username && (
+                <>
+                    <Link to="/Create">Create new post</Link>
+                    <a onClick={logout}>Logout</a>
+                </>
+            )}
+            {!username && (
+                <>
+                    <Link to="/login">Login</Link>
+                    <Link to="/register">Register</Link>
+                </>
+            )}
         </nav>
         </header>
     )
