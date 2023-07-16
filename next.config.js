afef96bee02075a3d0c0f0f5c996530754b2b3a9
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-07-17 00:25:43 +0300

    Added other files

---
+++ next.config.js
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    experimental: {
+        appDir: true,
+        serverComponentsExternalPackages: ["mongoose"],
+    },
+    images: {
+        remotePatterns: [
+            { hostname: 'res.cloudinary.com', protocol: 'https', port: '' }
+        ]
+    },
+    webpack(config) {
+        config.experiments = {
+            ...config.experiments,
+            topLevelAwait: true,
+        }
+        return config
+    }
+}
+
+module.exports = nextConfig
