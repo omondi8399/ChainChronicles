a974731c052164c2d10fc6bd19a5e07e84e5670b
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-07-17 05:13:18 +0300

    updated Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
     <div className={classes.container}>
       <div className={classes.wrapper}>
         <h2 className={classes.left}>
-          <Link href="/">ChainChronicle</Link>
+          <Link href="/">ChainChronicles</Link>
         </h2>
         <ul className={classes.right}>
           {
