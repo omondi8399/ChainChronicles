7f6de5778a9c78fd09e26d7718389dde4365330e
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-07-19 08:12:23 +0300

    Fixing responsive design for all routes

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import classes from './footer.module.css'
+import React from "react";
+import classes from "./footer.module.css";
 
 const Footer = () => {
   return (
@@ -8,26 +8,29 @@ const Footer = () => {
         <div className={classes.col}>
           <h2>About the App</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Voluptates velit fuga perspiciatis itaque iste, aliquid dignissimos voluptate modi,
-            tempore assumenda adipisci dolor hic atque quod consequuntur cupiditate. Quasi, nobis veritatis!
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates
+            velit fuga perspiciatis itaque iste, aliquid dignissimos voluptate
+            modi, tempore assumenda adipisci dolor hic atque quod consequuntur
+            cupiditate. Quasi, nobis veritatis!
           </p>
         </div>
-        <div className={classes.col}>
-          <h2>Contact</h2>
-          <span>Phone +254 707 606 757</span>
-          <span>Twitter: DevRojas</span>
-          <span>Github: omondi8399</span>
-        </div>
-        <div className={classes.col}>
-          <h2>Location</h2>
-          <span>Continent: Africa</span>
-          <span>Country: Kenya</span>
-          <span>Current Location: Kenya</span>
+        <div className={classes.content}>
+          <div className={classes.col}>
+            <h2>Contact</h2>
+            <span>Phone +254 707 606 757</span>
+            <span>Twitter: DevRojas</span>
+            <span>Github: omondi8399</span>
+          </div>
+          <div className={classes.col}>
+            <h2>Location</h2>
+            <span>Continent: Africa</span>
+            <span>Country: Kenya</span>
+            <span>Current Location: Kenya</span>
+          </div>
         </div>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
