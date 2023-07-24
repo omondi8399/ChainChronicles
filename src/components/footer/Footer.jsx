10384b97c416730c541a630ac4ed0a92d11329a2
Peter Njuguna <Ptahbn@gmail.com>
2023-07-24 19:02:26 +0300

    Modify footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,50 +1,31 @@
-import React from 'react';
-import { FaPhone, FaTwitter, FaGithub } from 'react-icons/fa';
+import { ImPhone, ImTwitter, ImGithub } from 'react-icons/im';
+import Link from 'next/link';
+import Newsletter from '../misc/Newsletter';
 
-const Footer = () => {
+export default function Footer() {
+
+  const bg = {
+    backgroundImage: "url('/images/footer.png')",
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'bottom left',
+  }
+
   return (
-    <footer className="bg-gray-800 text-white">
-      <div className="container mx-auto py-8 px-4 lg:px-8">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          <div className="col mb-8">
-            <h2 className="text-xl font-bold text-green-500">ChainChronicles</h2>
-            <p className="mt-4">
-              Welcome to ChainChronicles, <br /> the community-driven app that offers a diverse collection of captivating articles. Discover and share knowledge with like-minded individuals,
-              as we unite through the art of storytelling.
-            </p>
+    <footer className="bg-gray-800" style={bg}>
+      <Newsletter />
+      <div className="container mx-auto flex justify-center py-8">
+        <div className="py-8">
+          <div className="flex gap-6 justify-center">
+            <Link href={"/"}><ImPhone className="text-gray-300 hover:text-white cursor-pointer" size={24} /></Link>
+            <Link href={"https://twitter.com/DevRojas"} target={"_blank"} rel={"noopener noreferrer"}><ImTwitter className="text-gray-300 hover:text-white cursor-pointer" size={24} /></Link>
+            <Link href={"https://github.com/omondi8399"} target={"_blank"} rel={"noopener noreferrer"}><ImGithub className="text-gray-300 hover:text-white cursor-pointer" size={24} /></Link>
           </div>
-          <div className="col mb-8">
-            <h2 className="text-xl font-bold">Contact</h2>
-            <div className="mt-4">
-              <span className="block">
-                <FaPhone className="inline-block text-blue-500" /> +254 707 606 757
-              </span>
-              <span className="block">
-                <FaTwitter className="inline-block text-blue-500" />{' '}
-                <a href="https://twitter.com/DevRojas" target="_blank" rel="noopener noreferrer">
-                  DevRojas
-                </a>
-              </span>
-              <span className="block">
-                <FaGithub className="inline-block text-blue-500" />{' '}
-                <a href="https://github.com/omondi8399" target="_blank" rel="noopener noreferrer">
-                  omondi8399
-                </a>
-              </span>
-            </div>
-          </div>
-          <div className="col mb-8">
-            <h2 className="text-xl font-bold">Location</h2>
-            <div className="mt-4">
-              <span className="block">Continent: Africa</span>
-              <span className="block">Country: Kenya</span>
-              <span className="block">Current Location: Kenya</span>
-            </div>
-          </div>
+
+          <p className='py-2 text-gray-400 text-center'>@ 2023. All rights reserved.</p>
+          <p className='text-gray-400 text-center'> Welcome to ChainChronicles </p>
+          <p className='text-gray-400 text-center'> Location: Nairobi, Kenya</p>
         </div>
       </div>
     </footer>
-  );
-};
-
-export default Footer;
+  )
+}
