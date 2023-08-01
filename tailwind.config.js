68aff478ab50d514e963de16b9059f3ece5fc030
Peter Njuguna <Ptahbn@gmail.com>
2023-08-01 16:16:58 +0300

    Install dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,17 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        float: "float 3s ease-in-out infinite",
+      },
+      keyframes: {
+        float: {
+          "0%, 100%": { transform: "translateY(0)" },
+          "50%": { transform: "translateY(-10%)" },
+        },
+      },
+    },
   },
   plugins: [],
 }
